e62cfa08172710490cf8c78a6b6b1e6e5598e8d8
Lukas Scheuerle <lukas.scheuerle@web.de>
2019-10-02 12:38:26 +0200

    more types. nearing completion.

--- index.d.ts
+++ index.d.ts
@@ -20,6 +20,25 @@ declare module "minecraft-realms" {
     getBackup(id: any): Backup;
   }
 
+  export class Client {
+    client: RealmsClient;
+    constructor(accessToken: string, version: string, username: string);
+    get trial(): boolean;
+    get availability(): boolean;
+    get compatible(): "COMPATIBLE" | "OUTDATED" | "OTHER";
+    get invitesCount(): number;
+    get worlds(): RealmsServerList;
+    get news(): RealmsNews;
+    /**
+     * @param type The type of the Template `MINIGAME`, `ADVENTUREMAP`, `NORMAL`, `EXPERIENCE`, or `INSPIRATION`.
+     * @param page 
+     * @param size 
+     */
+    templates(type: string, page: number, size: number): RealmsTemplatePaginatedList;
+    get invites(): PendingInvitesList;
+    get activities(): any;
+  }
+
   export interface Metadata {
     game_difficulty: any;
     name: any,
@@ -51,37 +70,44 @@ declare module "minecraft-realms" {
     //TODO: This is where is stopped working
   }
 
-  export class Client {
-    constructor(accessToken: string, version: string, username: string);
-    get trial(): boolean;
-    get availability(): boolean;
-    get compatible(): "COMPATIBLE" | "OUTDATED" | "OTHER";
-    get invitesCount(): number;
-    get worlds(): RealmsServerList;
-    get news(): RealmsNews;
-    /**
-     * @param type The type of the Template `MINIGAME`, `ADVENTUREMAP`, `NORMAL`, `EXPERIENCE`, or `INSPIRATION`.
-     * @param page 
-     * @param size 
-     */
-    templates(type: string, page: number, size: number): RealmsTemplatePaginatedList;
-    get invites(): PendingInvitesList;
-    get activities(): any;
+  export class PendingInvitesList extends ValueObject {
+    client: Client;
+    invites: PendingInvite[];
+    constructor(unparsedJSON: string, client: Client);
+    getInvite(id: number): PendingInvite;
   }
 
+  export class PlayerInfo extends ValueObject {
+    world: RealmsServer;
+    client: Client;
+    name: string;
+    uuid: string;
+    operator: boolean;
+    accepted: boolean;
+    online: boolean;
+
+    constructor(parsedJSON: any, realmsServer: RealmsServer);
+    makeOp(): Ops;
+    deopPlayer(): Ops;
+    kickPlayer(): RealmsServer;
+  }
+
+  export class PlayerList extends ValueObject {
+    players: PlayerInfo[];
+  }
+
+
   export class RealmsClient {
-    /**
-     * Main class for control Realms
-     * @param {String} accessToken 
-     * @param {String} version 
-     * @param {String} username 
-     */
+    accessToken: string;
+    uuid: string;
+    username: string;
+    version: string;
     constructor(accessToken: string, version: string, username: string);
     //GET requests
     checkAvailable(): boolean;
     checkCompatiables(): boolean;
     invitesCount(): number;
-    invites(): any;
+    invites(): string;
     trial(): boolean;
     news(): RealmsNews;
     activities(): string;
@@ -106,29 +132,208 @@ declare module "minecraft-realms" {
     //POST Requests
     makeOp(worldId: number, UUID: string): any;
     invitePlayer(worldId: number, username: string): any;
-    resetWorld(worldId: number, realmsworldresetdto: RealmsDesctiptionDto): any;
-    setDesctiption(realmsdesctiptiondto: RealmsDesctiptionDto);
+    resetWorld(worldId: number, realmsworldresetdto: RealmsDescriptionDto): any; //is this actually a RealmsDescriptionDto or a RealmsWorldResetDto?
+    setDesctiption(realmsdesctiptiondto: RealmsDescriptionDto);
   }
 
-  interface RealmsNews {
-    client: any;
+  export class RealmsDescriptionDto extends ValueObject {
+    name: string;
+    description: string;
+    world: RealmsServer;
+    constructor(name: string, description: string, world: RealmsServer);
+    setName(name: string): RealmsServer;
+    setDescription(description: string): RealmsServer;
+    setProperties(name: string, description: string): RealmsServer;
+  }
+
+  export class RealmsNews extends ValueObject {
+    client: Client;
     newsLink: string;
+    constructor(unparsedJSON: string, client: Client);
   }
 
-  enum Status {
+  export class RealmsServer {
+    client: Client;
+    id: number;
+    remoteSubscriptionId: number;
+    owner: string;
+    ownerUUID: string;
+    properties: RealmsDescriptionDto;
+    defaultPermission: string;
+    state: string;  //probably Status
+    daysLeft: number;
+    expired: boolean;
+    expiredTrial: boolean;
+    gracePeriod: boolean;
+    worldType: string; // probably Template
+    players: PlayerInfo[];
+    maxPlayers: number;
+    minigameName: string;
+    minigameId: number;
+    minigameImage: string;
+    activeSlot: number; //probably SlotNumber
+    slots: Map<number, RealmsWorldOptions>;
+    member: boolean;
+    clubId: number;
+    constructor(unparsedJSON: string, client: Client);
+    sortPlayers(): RealmsServer;
+    detailInformation(): RealmsServer;
+    invitePlayer(username: string): RealmsServer;
+    getPlayerByUUID(uuid: string): PlayerInfo;
+    getPlayerByName(name: string): PlayerInfo;
+    get joinCreditails(): RealmsServerAddress;
+    changeSlot(slot: SlotNumber): RealmsServer;
+    open(): boolean;
+    close(): boolean;
+    download(slot: SlotNumber): WorldDownload;
+    downloadActiveSlot(): WorldDownload;
+    upload(): UploadInfo;
+    setMinigame(minigameId: number | string): boolean;
+    backups(): BackupList;
+    setTemplate(id: number): boolean;
+    createNewWorld(seed: string, levelType: any, generateStructures: boolean): any;
+  }
+
+  export class RealmsServerAddress extends ValueObject {
+    address: string;
+    resourcePackUrl: string;
+    resourcePackHash: string;
+    constructor(unparsedJSON: string);
+  }
+
+  export class RealmsServerList {
+    servers: RealmsServer[];
+    client: Client;
+    constructor(unparsedJSON: string, client: Client);
+    getWorld(id: number): RealmsServer;
+  }
+
+  export class RealmsWorldOptions extends ValueObject {
+    pvp: boolean;
+    spawnAnimals: boolean;
+    spawnMonsters: boolean;
+    spawnNPCs: boolean;
+    spawnProtection: number;
+    commandBlocks: boolean;
+    difficulty: Difficulty;
+    gameMode: GameMode;
+    forceGamemode: boolean;
+    slotName: string;
+    templateId: number;
+    templateImage: string;
+    adventureMap: boolean;
+    empty: boolean;
+    // the rest is in the JSON but isn't saved.
+    // resourcePackHash: string;
+    // incompatibilities: any[];
+    // versionRef: string;
+    // versionLock: string;
+    // cheatsAllowed: boolean;
+    // texturePacksRequired: boolean;
+    // enabledPacks: {
+    //   resourcePacks: any[];
+    //   behaviorPacks: any[];
+    // };
+    // customGameServerGlobalProperties: any;
+
+    constructor(pvp: boolean, spawnAnimals: boolean, spawnMonsters: boolean, spawnNPCs: boolean, spawnProtection: boolean, commandBlocks: boolean, difficulty: Difficulty, gameMode: GameMode, forceGameMode: boolean, slotName: string)
+    static parse(unparsedJSON: string): RealmsWorldOptions;
+    static getEmptyDefaults(): RealmsWorldOptions;
+    setEmpty(empty: boolean): void;
+  }
+
+  export class RealmsWorldResetDto extends ValueObject {
+    constructor(seed: string, worldTemplateId: number, levelType: any, generateStructures: boolean);
+    seed: string;
+    worldTemplateId: number;
+    levelType: any; // probably either WorldType or Templates
+    generateStructures: boolean;
+  }
+
+  export class Request {
+    token: string;
+    uuid: string;
+    version: string;
+    username: string;
+    endpoint: string;
+    constructor(token: string, uuid: string, version: string, username: string);
+    get cookieHeader(): string;
+    get(url: string): string;
+    post(url: string, payload: any): string;
+    put(url: string): string;
+    delete(url: string): string;
+  }
+
+  export class UploadInfo extends ValueObject {
+    worldClosed: boolean;
+    token: string;
+    uploadEndpoint: any;
+    port: any;
+    constructor(unparsedJSON: string);
+  }
+
+  export class ValueObject {
+    toString(): string;
+    toJSON(): JSON;
+  }
+
+  export class WorldDownload extends ValueObject {
+    downloadLink: string;
+    resourcePackUrl: string;
+    resourcePackHash: string;
+    constructor(unparsedJSON: string);
+  }
+
+  export class WorldTemplate extends ValueObject {
+    id: number;
+    name: string;
+    version: string;
+    author: string;
+    image: string;
+    trailer: string;
+    recommendedPlayers: string;
+    type: Templates;
+    constructor(parsedJSON: any);
+  }
+
+  export class WorldTemplatePaginatedList extends ValueObject {
+    size: number;
+    page: number;
+    total: number;
+    templates: WorldTemplate[];
+    constructor(unparsedJSON: string);
+    getTemplate(id: number): WorldTemplate;
+  }
+
+
+  export enum GameMode {
+    SURVIVAL = 0,
+    CREATIVE = 1,
+    ADVENTURE = 2,
+    SPECTATOR = 3
+  }
+
+  export enum Difficulty {
+    PEACEFUL = 0,
+    EASY = 1,
+    NORMAL = 2,
+    HARD = 3
+  }
+
+  export enum Status {
     CLOSED = "CLOSED",
     OPEN = "OPEN",
     UNINITIALIZED = "UNINITIALIZED"
   }
 
-  enum WorldType {
+  export enum WorldType {
     DEFAULT = 0,
     FLAT = 1,
     LARGE_BIOMES = 2,
     AMPLIFIED = 3
   }
 
-  enum Templates {
+  export enum Templates {
     ADVENTURES = "ADVENTUREMAP",
     WORLD_TEMPLATES = "NORMAL",
     MINIGAMES = "MINIGAME",
@@ -136,34 +341,6 @@ declare module "minecraft-realms" {
     INSPIRATION = "INSPIRATION"
   }
 
-  type SlotNumber = 1 | 2 | 3 | 4;
+  export type SlotNumber = 1 | 2 | 3 | 4;
 
-  class RealmsServerList {
-    servers: RealmsServer[];
-    getWorld(id: number): RealmsServer;
-  }
-
-  class RealmsServer {
-    sortPlayers(): RealmsServer;
-    detailInformation(): RealmsServer;
-    invitePlayer(username: string): RealmsServer;
-    getPlayerByUUID(uuid: string): any;
-    getPlayerByName(name: string): any;
-    get joinCreditails(): RealmsServerAddress;
-    changeSlot(slot: SlotNumber): any;
-    open(): boolean;
-    close(): boolean;
-    download(slot: SlotNumber): any;
-    downloadActiveSlot(): any;
-    upload(): UploadInfo;
-    setMinigame(minigameId: number | string): boolean;
-    backups(): BackupList;
-    setTemplate(id: number): boolean;
-    createNewWorld(seed: string, levelType: any, generateStructures: boolean);
-  }
-
-  class ValueObject {
-    toString(): string;
-    toJSON(): any;
-  }
 }
